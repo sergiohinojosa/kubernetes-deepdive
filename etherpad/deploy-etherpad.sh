250528366d326feca11e43057d1d1a066731c929
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-04-16 11:28:43 +0200

    anpassung domain in etherpad, adding ssl

--- etherpad/deploy-etherpad.sh
+++ etherpad/deploy-etherpad.sh
@@ -4,8 +4,12 @@
 # Application https://github.com/ether/etherpad-lite/blob/develop/doc/docker.md
 
 # Set the public ip for the ingress rule.
+
 export PUBLIC_IP=$(curl -s ifconfig.me) 
-sed "s/PUBLIC_IP/$PUBLIC_IP/g" 03-etherpad-ingress.template > 03-etherpad-ingress.yaml
+PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
+export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
+sed 's~domain.placeholder~'"$DOMAIN"'~' 03-etherpad-ingress.template > 03-etherpad-ingress.yaml
+
 kubectl apply -f .
 echo "Etherpad service available at:"
 kubectl get ing etherpad-ingress
