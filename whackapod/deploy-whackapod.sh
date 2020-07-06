99bab842235d0c885264a1d43247421d8b3fdb91
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-07-06 11:47:58 +0200

    change rights exec

--- whackapod/deploy-whackapod.sh
+++ whackapod/deploy-whackapod.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-export PUBLIC_IP=$(curl -s ifconfig.me) 
-PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
-export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
+if [ $# -eq 1 ]; then
+    # Read JSON and set it in the CREDS variable 
+    DOMAIN=$1
+    echo "Domain passed $DOMAIN"
+else
+   export PUBLIC_IP=$(curl -s ifconfig.me) 
+   PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
+   export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
+fi
 
 sed 's~domain.placeholder~'"$DOMAIN"'~'  ingress.template > manifests/07-ingress.yaml
 
