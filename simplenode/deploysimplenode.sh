0b6958f778502d0b274e6106aee819a337022640
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-08-31 18:35:26 +0200

    simplify deployment of simplenode

--- simplenode/deploysimplenode.sh
+++ simplenode/deploysimplenode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Create namespace workshop
 kubectl create ns workshop
 
@@ -7,3 +8,13 @@ kubectl -n workshop create deploy simplenode --image=grabnerandi/simplenodeservi
 
 # Expose deployment of simplenode
 kubectl -n workshop expose deployment simplenode --type=NodePort --port=8080 --name simplenode
+
+## Get Public IP as NIP Domain
+export PUBLIC_IP=$(curl -s ifconfig.me) 
+PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
+export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
+
+## Create Ingress Rules
+sed 's~domain.placeholder~'"$DOMAIN"'~' ingress.template > manifests/ingress.yaml
+
+kubectl apply -f manifests/ingress.yaml
