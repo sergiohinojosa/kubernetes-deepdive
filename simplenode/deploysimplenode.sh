c8b6cc8a23558c5637e8a8de0ad891c9f58a62cb
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-08-31 18:32:43 +0200

    adding namespace to simplenode

--- simplenode/deploysimplenode.sh
+++ simplenode/deploysimplenode.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# Create namespace workshop
+kubectl create ns workshop
 
 # Create deployment of simplenode
-kubectl create deploy simplenode --image=grabnerandi/simplenodeservice:1.0.0
+kubectl -n workshop create deploy simplenode --image=grabnerandi/simplenodeservice:1.0.0 
 
 # Expose deployment of simplenode
-kubectl expose deployment simplenode --type=NodePort --port=8080 --name simplenode
+kubectl -n workshop expose deployment simplenode --type=NodePort --port=8080 --name simplenode
