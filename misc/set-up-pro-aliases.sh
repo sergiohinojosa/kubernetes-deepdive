596e3e20934b4dffe1d062fb8722244406d98097
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-12-03 10:09:50 +0100

    adding proaliases

---
+++ misc/set-up-pro-aliases.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This file contains the functions for installing Keptn-in-a-Box.
+# Each function contains a boolean flag so the installations
+# can be highly customized.
+
+setupProAliases() {
+  if [ "$setup_proaliases" = true ]; then
+    printInfoSection "Adding Bash and Kubectl Pro CLI aliases to .bash_aliases for user ubuntu and root "
+    echo "
+      # Alias for ease of use of the CLI
+      alias las='ls -las' 
+      alias hg='history | grep' 
+      alias h='history' 
+      alias vaml='vi -c \"set syntax:yaml\" -' 
+      alias vson='vi -c \"set syntax:json\" -' 
+      alias pg='ps -aux | grep' " >/root/.bash_aliases
+    homedir=$(eval echo ~$USER)
+    cp /root/.bash_aliases $homedir/.bash_aliases
+  fi
+}
